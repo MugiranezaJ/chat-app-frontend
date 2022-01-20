da585dfa749d31b236cd6b129e2ca5d8e8f6675f
MugiranezaJ <jackswalter7@gmail.com>
2022-01-20 23:58:09 +0200

    Adding register and login

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import react from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import { Home } from '../components/views/Home'
+import Login from '../components/views/Login'
+import Register from '../components/views/Register'
+
+export default function appRoutes(){
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/home' element={<Home/>} />
+                <Route path='/login' element={<Login/>}/>
+                <Route path='/register' element={<Register/>} />
+            </Routes>
+        </BrowserRouter>
+    )
+    
+}
